6f75974b7235a4a1f26b30c1f0f998cd50353681
Administrator <fereshtemohsenian@gmail.com>
2019-09-14 17:28:40 +0200

    add controllers

---
+++ ElavatorStatus/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Schindler.ElavatorStatus.Domain;
+
+namespace Schindler.ElavatorStatus.WebService.Controllers
+{
+    [ApiController]
+    [Route("v1/api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private IUserRepository _userService;
+
+        public UserController(IUserRepository userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+    }
+}
---
+++ ElavatorStatus/Controllers/V1/ElevatorController.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Schindler.ElavatorStatus.Domain;
+using Schindler.ElavatorStatus.Domain.Extensions;
+
+
+namespace Schindler.ElavatorStatus.WebService.Controllers.V1
+{
+
+    [Route("v1/api/[controller]")]
+    [ApiController]
+    public class ElevatorController : ControllerBase
+    {
+        private readonly IElavatoStatusRepository _elavatoStatusRepository;
+
+        public ElevatorController(IElavatoStatusRepository elavatoStatusRepository)
+        {
+            _elavatoStatusRepository = elavatoStatusRepository.IfNotNull();
+        }
+
+        [HttpPost("AddElavatorStatus")]
+        [Authorize(Roles = Role.Admin)]
+        public ActionResult Post(Schindler.ElavatorStatus.Domain.ElavatorStatus value)
+        {
+            _elavatoStatusRepository.InsertStatus(value);
+            return CreatedAtAction("Get", new { id = value.Id }, value);
+
+        }
+
+        [HttpGet("ElavatoStatus/{id}")]
+        public ActionResult<Schindler.ElavatorStatus.Domain.ElavatorStatus> Get(Guid id)
+        {
+            var item = _elavatoStatusRepository.GetElavatorStatus(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [HttpGet("ElavatoStatus")]
+        public ActionResult<IEnumerable<Schindler.ElavatorStatus.Domain.ElavatorStatus>> Get()
+        {
+            var items = _elavatoStatusRepository.GetStatuses();
+            return Ok(items);
+        }
+
+    }
+}
